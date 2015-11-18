4676b92bfe587a999d81f7292267e7d5d2dce2fd
Karlyn Rahmgren <krahmgren3@gmail.com>
2015-11-18 19:20:09 +0000

    Changes from Screen Builder

---
+++ NatAppV3/components/area/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+app.area = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+
+// START_CUSTOM_CODE_area
+// END_CUSTOM_CODE_area
---
+++ NatAppV3/components/plants/index.js
@@ -0,0 +1,65 @@
+'use strict';
+
+app.plants = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+
+// START_CUSTOM_CODE_plants
+// END_CUSTOM_CODE_plants
+(function(parent) {
+    var dataProvider = app.data.defaultProvider,
+        flattenLocationProperties = function(dataItem) {
+            var propName, propValue,
+                isLocation = function(value) {
+                    return propValue && typeof propValue === 'object' &&
+                        propValue.longitude && propValue.latitude;
+                };
+
+            for (propName in dataItem) {
+                if (dataItem.hasOwnProperty(propName)) {
+                    propValue = dataItem[propName];
+                    if (isLocation(propValue)) {
+                        dataItem[propName] =
+                            kendo.format('Latitude: {0}, Longitude: {1}',
+                                propValue.latitude, propValue.longitude);
+                    }
+                }
+            }
+        },
+        dataSourceOptions = {
+            type: 'everlive',
+            transport: {
+                typeName: 'Activities',
+                dataProvider: dataProvider
+            },
+
+            change: function(e) {
+                var data = this.data();
+                for (var i = 0; i < data.length; i++) {
+                    var dataItem = data[i];
+
+                    flattenLocationProperties(dataItem);
+                }
+            },
+            schema: {
+                model: {
+                    fields: {
+                        'Text': {
+                            field: 'Text',
+                            defaultValue: ''
+                        },
+                    }
+                }
+            },
+        },
+        dataSource = new kendo.data.DataSource(dataSourceOptions),
+        plantsModel = kendo.observable({
+            dataSource: dataSource
+        });
+
+    parent.set('plantsModel', plantsModel);
+})(app.plants);
+
+// START_CUSTOM_CODE_plantsModel
+// END_CUSTOM_CODE_plantsModel
