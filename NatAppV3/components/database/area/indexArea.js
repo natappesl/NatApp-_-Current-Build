1fdfbdfbcbafc6a7e195720a3ae8b0c01c27fd57
Naturalist App <natappesl@ucmerced.edu>
2016-09-07 18:07:07 +0000

    Changes from Views service

--- NatAppV3/components/database/area/indexArea.js
+++ NatAppV3/components/database/area/indexArea.js
@@ -1,6 +1,17 @@
-'use strict';
+//'use strict';
 
-app.area = kendo.observable({
-    onShow: function() {},
-    afterShow: function() {}
-});
+//app.area = kendo.observable({
+    //onShow: function() {},
+    //afterShow: function() {}
+//});
+function initMap() {
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    // Set mapTypeId to google.maps.MapTypeId.SATELLITE in order
+    // to activate satellite imagery.
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+    scrollwheel: false,
+    zoom: 8
+  });
+}
