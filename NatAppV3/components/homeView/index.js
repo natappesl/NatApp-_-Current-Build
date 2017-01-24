405cb717938b2f2953d5c72a354fe043b0403547
Naturalist App <natappesl@ucmerced.edu>
2017-01-24 21:10:49 +0000

    Changes from Views service

--- NatAppV3/components/homeView/index.js
+++ NatAppV3/components/homeView/index.js
@@ -9,46 +9,6 @@ app.homeView = kendo.observable({
 // END_CUSTOM_CODE_homeView
 (function(parent) {
     var dataProvider = app.data.defaultProvider,
-        fetchFilteredData = function(paramFilter, searchFilter) {
-            var model = parent.get('homeViewModel'),
-                dataSource = model.get('dataSource');
-
-            if (paramFilter) {
-                model.set('paramFilter', paramFilter);
-            } else {
-                model.set('paramFilter', undefined);
-            }
-
-            if (paramFilter && searchFilter) {
-                dataSource.filter({
-                    logic: 'and',
-                    filters: [paramFilter, searchFilter]
-                });
-            } else if (paramFilter || searchFilter) {
-                dataSource.filter(paramFilter || searchFilter);
-            } else {
-                dataSource.filter({});
-            }
-        },
-        processImage = function(img) {
-
-            function isAbsolute(img) {
-                if  (img && (img.slice(0,  5)  ===  'http:' || img.slice(0,  6)  ===  'https:' || img.slice(0,  2)  ===  '//'  ||  img.slice(0,  5)  ===  'data:')) {
-                    return true;
-                }
-                return false;
-            }
-
-            if (!img) {
-                var empty1x1png = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVQI12NgYAAAAAMAASDVlMcAAAAASUVORK5CYII=';
-                img = 'data:image/png;base64,' + empty1x1png;
-            } else if (!isAbsolute(img)) {
-                var setup = dataProvider.setup || {};
-                img = setup.scheme + ':' + setup.url + setup.appId + '/Files/' + img + '/Download';
-            }
-
-            return img;
-        },
         flattenLocationProperties = function(dataItem) {
             var propName, propValue,
                 isLocation = function(value) {
@@ -70,9 +30,10 @@ app.homeView = kendo.observable({
         dataSourceOptions = {
             type: 'everlive',
             transport: {
-                typeName: 'Animal',
+                typeName: 'Activities',
                 dataProvider: dataProvider
             },
+
             change: function(e) {
                 var data = this.data();
                 for (var i = 0; i < data.length; i++) {
@@ -81,119 +42,20 @@ app.homeView = kendo.observable({
                     flattenLocationProperties(dataItem);
                 }
             },
-            error: function(e) {
-
-                if (e.xhr) {
-                    alert(JSON.stringify(e.xhr));
-                }
-            },
             schema: {
                 model: {
                     fields: {
-                        'Ave_Height': {
-                            field: 'Ave_Height',
+                        'Text': {
+                            field: 'Text',
                             defaultValue: ''
                         },
                     }
                 }
             },
-            serverFiltering: true,
-            serverSorting: true,
-            sort: {
-                field: 'CreatedAt',
-                dir: 'asc'
-            },
         },
         dataSource = new kendo.data.DataSource(dataSourceOptions),
-        // start data sources
-        // end data sources
         homeViewModel = kendo.observable({
-            dataSource: dataSource,
-            fixHierarchicalData: function(data) {
-                var result = {},
-                    layout = {};
-
-                $.extend(true, result, data);
-
-                (function removeNulls(obj) {
-                    var i, name,
-                        names = Object.getOwnPropertyNames(obj);
-
-                    for (i = 0; i < names.length; i++) {
-                        name = names[i];
-
-                        if (obj[name] === null) {
-                            delete obj[name];
-                        } else if ($.type(obj[name]) === 'object') {
-                            removeNulls(obj[name]);
-                        }
-                    }
-                })(result);
-
-                (function fix(source, layout) {
-                    var i, j, name, srcObj, ltObj, type,
-                        names = Object.getOwnPropertyNames(layout);
-
-                    for (i = 0; i < names.length; i++) {
-                        name = names[i];
-                        srcObj = source[name];
-                        ltObj = layout[name];
-                        type = $.type(srcObj);
-
-                        if (type === 'undefined' || type === 'null') {
-                            source[name] = ltObj;
-                        } else {
-                            if (srcObj.length > 0) {
-                                for (j = 0; j < srcObj.length; j++) {
-                                    fix(srcObj[j], ltObj[0]);
-                                }
-                            } else {
-                                fix(srcObj, ltObj);
-                            }
-                        }
-                    }
-                })(result, layout);
-
-                return result;
-            },
-            itemClick: function(e) {
-                var dataItem = e.dataItem || homeViewModel.originalItem;
-
-                app.mobileApp.navigate('#components/homeView/details.html?uid=' + dataItem.uid);
-
-            },
-            detailsShow: function(e) {
-                homeViewModel.setCurrentItemByUid(e.view.params.uid);
-            },
-            setCurrentItemByUid: function(uid) {
-                var item = uid,
-                    dataSource = homeViewModel.get('dataSource'),
-                    itemModel = dataSource.getByUid(item);
-
-                if (!itemModel.Ave_Height) {
-                    itemModel.Ave_Height = String.fromCharCode(160);
-                }
-
-                homeViewModel.set('originalItem', itemModel);
-                homeViewModel.set('currentItem',
-                    homeViewModel.fixHierarchicalData(itemModel));
-
-                return itemModel;
-            },
-            linkBind: function(linkString) {
-                var linkChunks = linkString.split('|');
-                if (linkChunks[0].length === 0) {
-                    return this.get("currentItem." + linkChunks[1]);
-                }
-                return linkChunks[0] + this.get("currentItem." + linkChunks[1]);
-            },
-            imageBind: function(imageField) {
-                if (imageField.indexOf("|") > -1) {
-                    return processImage(this.get("currentItem." + imageField.split("|")[0]));
-                }
-                return processImage(imageField);
-            },
-            currentItem: {}
+            dataSource: dataSource
         });
 
     if (typeof dataProvider.sbProviderReady === 'function') {
@@ -203,26 +65,6 @@ app.homeView = kendo.observable({
     } else {
         parent.set('homeViewModel', homeViewModel);
     }
-
-    parent.set('onShow', function(e) {
-        var param = e.view.params.filter ? JSON.parse(e.view.params.filter) : null,
-            isListmenu = true,
-            backbutton = e.view.element && e.view.element.find('header [data-role="navbar"] .backButtonWrapper');
-
-        if (param || isListmenu) {
-            backbutton.show();
-            backbutton.css('visibility', 'visible');
-        } else {
-            if (e.view.element.find('header [data-role="navbar"] [data-role="button"]').length) {
-                backbutton.hide();
-            } else {
-                backbutton.css('visibility', 'hidden');
-            }
-        }
-
-        fetchFilteredData(param);
-    });
-
 })(app.homeView);
 
 // START_CUSTOM_CODE_homeViewModel
